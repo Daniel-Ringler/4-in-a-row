978a8ae597b44d05269c02933aba5b2867dfd0ac
Daniel <Daniel@Apollo>
2019-07-05 14:23:30 +0200

    redo_test_commit

--- main.py
+++ main.py
@@ -323,21 +323,3 @@ class Game_Logic:
         
          
 PgView().run()                                                                                                                                      
-
-
-'''test import unittest
-
-class TestCase(unittest.TestCase):
-
-    def setUp(self):
-        unittest.TestCase.setUp(self)
-
-    def tearDown(self):
-        unittest.TestCase.tearDown(self)
-
-    def testMet1(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
-'''
