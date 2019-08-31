a5decf451254b581e1f913aa5784ec0971450db9
Daniel <Daniel@Apollo>
2019-08-31 16:38:48 +0200

    added color variables; added support for asd keys; added a aftergame menu; fixed a bug in check_win

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ author: Daniel Ringler
 works with pyhton3.7"""
 
 import pygame
+from pygame import display
 
 
 class PgView:
@@ -14,6 +15,9 @@ class PgView:
         pygame.font.init()
         pygame.display.set_caption("4 in a Row")
         self.white = (255,255,255)
+        self.black = (0,0,0)
+        self.red = (255,0,0)
+        self.yellow = (252,240,10)
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
@@ -21,7 +25,6 @@ class PgView:
         self.game_surface.fill((self.white))
         self.menu_surface = pygame.Surface(self.screen.get_size()).convert()
         self.menu_surface.fill(self.white)
-    
     
     def draw_gameboard(self):
         x = 50
@@ -38,26 +41,98 @@ class PgView:
                 x = 50
                 y -= 100
     
-    
     def draw_token(self, counter, location):
         if counter % 2 == 0:
-            token = Tokens((255,0,0), (location))
+            token = Tokens((self.red), (location))
         else:
-            token = Tokens((255,255,0), (location))
+            token = Tokens((self.yellow), (location))
             
         token.blit(self.game_surface)
     
     def draw_whitespace(self):
         surface = pygame.Surface((800,150))
-        surface.fill((255,255,255))
+        surface.fill((self.white))
         surface = surface.convert()
         self.game_surface.blit(surface, (0,0))     
         
-    def draw_font(self, text):
-        font = pygame.font.SysFont('Arial', 30)
-        textsurface = font.render(text, False, (0, 0, 0))
-        self.game_surface.blit(textsurface, (0,0))
+    def create_font(self, text, color, size):
+        font = pygame.font.SysFont('Arial', size)
+        textsurface = font.render(text, False, (color))
+        return textsurface
+        
+    def draw_menu(self):
+        textsurface_menu_header = self.create_font("4 in  a Row", (self.red), 50)
+        textsurface_menu_text = self.create_font("Press SPACE to start playing !", (self.yellow), 40)
+        textsurfaces_menu = [textsurface_menu_header, textsurface_menu_text]
+        
+        counter = 0
+        
+        for textsurface in textsurfaces_menu:
+            x_centered = self.text_centered_x(textsurface)
+            width, height = textsurface.get_size()
+            if counter == 0:
+                y_centered = (800/2-100)-height
+            else:
+                y_centered = (800/2-100)+height
+            counter += 1
+            self.menu_surface.blit(textsurface, (x_centered, y_centered))
     
+    def draw_game(self, counter, x, y, token_array, win):
+        self.draw_whitespace()
+        if win == False: self.draw_token(counter, (x,y))
+        self.draw_gameboard()
+        
+        # draw tokens on the gameboard
+        for i in token_array:
+            position = i
+            if len(position) == 3:
+                token_color = position[2]
+                position.pop()
+            self.draw_token(token_color, position)
+            position.append(token_color)
+
+        self.screen.blit(self.game_surface, (0,0))
+        pygame.display.update()
+        
+    def draw_endgame(self, win):
+        if win == "Red": color = self.red
+        else: color = self.yellow
+        
+        textsurface_won = self.create_font(str(win) + " has won !", color, 40)
+        textsurface_newround = self.create_font("press SPACE to start a new round", color, 30)
+        textsurface_quit = self.create_font("press ESC to quit playing", color, 30)
+        
+        textsurfaces_endgame = [textsurface_won, textsurface_newround, textsurface_quit]
+        y = 5
+        
+        for textsurface in textsurfaces_endgame:
+            centered_x = self.text_centered_x(textsurface)
+            self.game_surface.blit(textsurface, (centered_x, y))
+            _, height = textsurface.get_size()
+            y = y + height + 5
+        
+    def handle_win(self, win):
+        self.draw_endgame(win)
+        self.screen.blit(self.game_surface, (0,0))
+        display.flip()
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return 2
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        return 1
+                    if event.key == pygame.K_ESCAPE:
+                        return 2
+
+    
+    def text_centered_x(self, textsurface):
+        width, height = textsurface.get_size() # get the size of the textsurface
+        centered_x = (800/2)-(width/2) # calculates the x value for a x centered textsurface
+        return centered_x
+        
+
     def run(self):
         
         running = True
@@ -85,6 +160,7 @@ class PgView:
                             current_surface = "game"
                 
                 self.draw_whitespace()
+                self.draw_menu()
                 self.screen.blit(self.menu_surface, (0,0))
                 pygame.display.update()
                 
@@ -98,13 +174,13 @@ class PgView:
                             if event.key == pygame.K_BACKSPACE:
                                 current_surface = "menu"
                                 counter = 43
-                            if event.key == pygame.K_RIGHT:
+                            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                                 if x < 600:
                                     x += 100
-                            if event.key == pygame.K_LEFT:
+                            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                                 if x > 100:
                                     x -= 100
-                            if event.key == pygame.K_DOWN:
+                            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                                 y += 650
                                 a = Game_Logic(x, y, counter)
                                 token_check = a.compare_token_to_array(token_array)
@@ -123,35 +199,12 @@ class PgView:
                             if event.key == pygame.K_ESCAPE:                            
                                 running = False
                                 counter = 43
-                            
-                    self.draw_whitespace()
-                    self.draw_token(counter, (x,y))
-                    self.draw_gameboard()
+                     
+                    self.draw_game(counter, x, y, token_array, win)
                     
-                    # draw tokens on the gameboard
-                    for i in token_array:
-                        position = i
-                        if len(position) == 3:
-                            token_color = position[2]
-                            position.pop()
-                        self.draw_token(token_color, position)
-                        position.append(token_color)
-
-                    self.screen.blit(self.game_surface, (0,0))
-                    pygame.display.update()
-                                    
                     if win != False:
-                        print(str(win) + " has won!")
-                        print("Enter 1 to start a new round.")
-                        print("Enter 2 to quit playing")
-                        choice = int(input()) 
-
-                        while choice > 2 or choice < 1:
-                            print(str(choice) + " is not allowed.")
-                            print("Enter 1 to start a new round.")
-                            print("Enter 2 to quit playing.")
-                            choice = int(input())
-                            
+                        choice = self.handle_win(win)
+            
                         if choice == 1:
                             counter = 0
                             token_array = []
@@ -159,12 +212,9 @@ class PgView:
                         if choice == 2:
                             counter = 43
                             running = False
-                
             
             if running == False:
-                print("game stopped")
-                pygame.quit()
-                
+                pygame.quit()        
                     
         
 # class for generating a Gameboard Segment
@@ -260,7 +310,10 @@ class Game_Logic:
         current_x_y = [self.x, self.y, token]
             
         # check to the right
+        counter = 0
         for _ in range(3):
+            if h_counter < counter:
+                break
             if current_x_y[0] < 650:
                 current_x_y[0] += 100
             else:
@@ -269,11 +322,15 @@ class Game_Logic:
                 if current_x_y[1] == i[1]:
                     if current_x_y == i:
                         h_counter+= 1
+            counter += 1
             
         current_x_y = [self.x, self.y, token]
             
         # check to the left
+        counter = 0
         for _ in range(3):
+            if h_counter < counter:
+                break
             if current_x_y[0] > 50:
                 current_x_y[0] -= 100
             else:
@@ -282,6 +339,7 @@ class Game_Logic:
                 if current_x_y[1] == i[1]:
                     if current_x_y == i:
                         h_counter += 1
+            counter += 1
             
         if h_counter >= 3:
             win = True
@@ -290,51 +348,70 @@ class Game_Logic:
         # check for right diagonal
         # check right down
         current_x_y = [self.x, self.y, token]
-        
+        counter = 0
+        d_right_up_counter = 0
         for _ in range(3):
+            if d_right_up_counter < counter:
+                break
             current_x_y[0] += 100
             current_x_y[1] += 100
             for i in array:
                 if current_x_y == i:
-                    d_right_counter += 1
+                    d_right_up_counter += 1
                     break
+            counter += 1
     
         # check left up
         current_x_y = [self.x, self.y, token]
-        
+        counter = 0
+        d_right_down_counter = 0
         for _ in range(3):
+            if d_right_up_counter < counter:
+                break
             current_x_y[0] -= 100
             current_x_y[1] -= 100
             for i in array:
                 if i == current_x_y:
-                    d_right_counter += 1
+                    d_right_down_counter += 1
                     break
+            counter += 1
         
+        d_right_counter = d_right_up_counter + d_right_down_counter
         if d_right_counter >= 3:
             win = True
         
         # check for left diagonal
         # check right up
         current_x_y = [self.x, self.y, token]
-        
+        counter = 0
+        d_left_up_counter = 0
         for _ in range(3):
+            if d_left_up_counter < counter:
+                break
             current_x_y[0] += 100
             current_x_y[1] -= 100 
             for i in array:
                 if i == current_x_y:
-                    d_left_counter += 1
+                    d_left_up_counter += 1
                     break
+            counter += 1
                 
         # check left down
         current_x_y = [self.x, self.y, token]
-        
+        counter = 0
+        d_left_down_counter = 0
         for _ in range(3):
+            if d_left_down_counter < counter:
+                break
             current_x_y[0] -= 100
             current_x_y[1] += 100
             for i in array:
                 if i == current_x_y:
-                    d_left_counter += 1
+                    d_left_down_counter += 1
                     break
+            counter += 1
+        
+        d_left_counter = d_left_up_counter + d_left_down_counter
         
         if d_left_counter >= 3:
             win = True
@@ -344,5 +421,5 @@ class Game_Logic:
         else:
             return False
         
-         
+
 PgView().run()                                                                                                                                      
